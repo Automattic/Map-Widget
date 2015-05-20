3b7620739a3af3ee58928c34c539f80448b99f99
Tarendai <contact@tomjn.com>
2015-05-21 00:58:30 +0100

    remove the settings page, if its needed use vcs to bring it back

--- plugin.php
+++ plugin.php
@@ -9,10 +9,7 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-//require_once( 'cftp_map_widget_settings.php' );
 require_once( 'cftp_map_widget.php' );
-
-//cftp_map_widget_settings::getInstance();
 
 add_action( 'widgets_init', function() {
 	register_widget( 'cftp_map_widget' );
