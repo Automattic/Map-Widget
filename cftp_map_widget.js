21c9c783634aff4f3849603367be8846d8fce6ee
Tarendai <contact@tomjn.com>
2015-05-21 01:09:37 +0100

    remove dead code

--- cftp_map_widget.js
+++ cftp_map_widget.js
@@ -1,11 +1,4 @@
 jQuery(document).ready(function() {
-	/*var mapOptions = {
-		center: new google.maps.LatLng(-34.397, 150.644),
-		zoom: 8
-	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"),
-		mapOptions);*/
-
 	jQuery('.cftp_map_widget_container').each( function ( index ) {
 		var el = jQuery( this );
 		var mlat = el.data('lat');
