61c6a1a1db61881cad6f34955fff1d81c42718c4
Tarendai <contact@tomjn.com>
2015-05-21 01:17:42 +0100

    bump the version and adjust the description and author

--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Map Widget
-Description: CFTP Google Map Widget
-Author: Tom J Nowell, Code For The People
-Version: 1.0
+Description: Google Map Widget
+Author: Tom J Nowell, Code For The People, Automattic
+Version: 1.1
 Author URI: http://codeforthepeople.com/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
